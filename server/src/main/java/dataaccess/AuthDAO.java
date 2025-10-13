53a8f7cf7b6a146b3bce5dba0e75008a32ce497e
graceteusch <68242698+graceteusch@users.noreply.github.com>
2025-10-13 17:03:20 -0600

    Made DAO interfaces and wrote the interface methods for them (just names/parameters).

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,17 @@
 package dataaccess;
 
-public class AuthDAO {
+import model.AuthData;
+import model.GameData;
+
+public interface AuthDAO {
+    // CREATE
+    void createAuth(AuthData auth) throws DataAccessException;
+
+    // READ
+    AuthData getAuth(String authToken) throws DataAccessException;
+
+    // UPDATE
+    // ????
+    // DELETE
+    void removeAuth(String authToken) throws DataAccessException;
 }
--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,24 @@
 package dataaccess;
 
-public class GameDAO {
+import model.GameData;
+
+import javax.xml.crypto.Data;
+import java.util.Collection;
+
+public interface GameDAO {
+    // CREATE
+    void createGame(GameData game) throws DataAccessException;
+
+    // READ (get information)
+    GameData getGame(int gameID) throws DataAccessException;
+
+    Collection<GameData> listGames();
+
+    String checkIfColorTaken(int gameID, String playerColor);
+
+    // UPDATE
+    void updateGame(int gameID, String playerColor, String username);
+
+    // DELETE
+    void removeGame(int gameID) throws DataAccessException;
 }
