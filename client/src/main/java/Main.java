c46b0b57ca10326b55cfc04be8194bbfa374ac4b
graceteusch <68242698+graceteusch@users.noreply.github.com>
2025-11-07 15:07:14 -0700

    Updated client Main function so it starts the server and runs the client REPL.

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,15 +1,23 @@
 import chess.*;
 import ui.PreloginClient;
-import ui.ServerFacade;
+import server.Server;
 
-//you will still need to start your server using the Main.main function when you manually run your client.
+// you will still need to start your server using the Main.main function when you manually run your client.
 public class Main {
     public static void main(String[] args) {
         System.out.println("♕ 240 Chess Client");
 
+        // start server
+        Server server = new Server();
+        int port = server.run(0);
 
-        String serverUrl = "http://localhost:" + "8080";
+        // run client
+        String serverUrl = "http://localhost:" + port;
         PreloginClient client = new PreloginClient(serverUrl);
         client.run();
+
+        // step server when REPL ends
+        server.stop();
+        System.out.println("Server stopped.");
     }
 }
