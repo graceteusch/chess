c1d3a621943f408ddfa3f07e9f4f4b280d2a0fe6
graceteusch <68242698+graceteusch@users.noreply.github.com>
2025-11-12 17:02:45 -0700

    Fixed server issues.

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,4 +1,6 @@
+
 import chess.*;
+import server.Server;
 import ui.Repl;
 
 // you will still need to start your server using the Main.main function when you manually run your client.
@@ -6,20 +8,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("♕ 240 Chess Client");
 
-
-//        // start server
-//        Server server = new Server();
-//        int port = server.run(0);
-//
-//        // run client
-//        String serverUrl = "http://localhost:" + port;
-//
-
-
-        String serverUrl = "http://localhost:8080";
+        Server server = new Server();
+        int port = server.run(0);
+        String serverUrl = "http://localhost:" + port;
 
         // create Repl object and run it
         new Repl(serverUrl).run();
-
     }
 }
